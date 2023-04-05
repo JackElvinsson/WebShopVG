0a08e6e7bf19b39b5062d95f11669e4643c8a9e7
JackElvinsson <jackelvinsson@gmail.com>
2023-04-05 23:22:13 +0200

    fixedFetchProducts

--- fetch_products.js
+++ fetch_products.js
@@ -2,11 +2,13 @@ function fetchProducts(category) {
     const gridContainer = document.querySelector('.grid-container');
     const url = `https://fakestoreapi.com/products/category/${category}`;
 
-    gridContainer.innerHTML = ''; //tömmer 
+    // Clear the grid container to prevent duplicate products
+    gridContainer.innerHTML = '';
 
     fetch(url)
         .then(res => res.json())
         .then(products => {
+            console.log(products);
             products.forEach(product => {
                 const productDiv = document.createElement('div');
                 productDiv.classList.add('product');
@@ -22,28 +24,36 @@ function fetchProducts(category) {
                 const productPrice = document.createElement('p');
                 productPrice.textContent = `$${product.price}`;
                 productDiv.appendChild(productPrice);
+
+                const addToCartButton = document.createElement('button');
+                addToCartButton.textContent = 'Add to cart';
+                addToCartButton.classList.add('buy-button');
+                addToCartButton.addEventListener('click', () => {
+                    // Get the current cart or create a new empty cart if it doesn't exist
+                    let cart = JSON.parse(localStorage.getItem('cart')) || {};
+
+                    // Check if the product is already in the cart
+                    if (cart[product.id]) {
+                        // If the product is already in the cart, increment the quantity
+                        cart[product.id].quantity += 1;
+                    } else {
+                        // If the product is not in the cart, add it with a quantity of 1
+                        cart[product.id] = {
+                            id: product.id,
+                            title: product.title,
+                            price: product.price,
+                            quantity: 1,
+                            image: product.image
+                        };
+                    }
+
+                    // Save the updated cart to local storage
+                    localStorage.setItem('cart', JSON.stringify(cart));
+                    alert(`${product.title} has been added to your cart!`);
+                });
+                productDiv.appendChild(addToCartButton);
+
                 gridContainer.appendChild(productDiv);
-
-                const item = {
-                    id: product.id, 
-                    title: product.title, 
-                    price: product.price,
-                    description: product.description, 
-                    category: product.category, 
-                    image: product.image
-                };
-
-                const productA = document.createElement('a');
-                productA.setAttribute('href', 'order.html');
-                productDiv.appendChild(productA);
-
-                const productButton = document.createElement('button')
-                productButton.textContent = "Köp";
-                productButton.classList.add('buy-button');
-                productButton.onclick = function () {
-                    order(item)
-                }  
-                productA.appendChild(productButton)
             });
         })
         .catch(error => {
